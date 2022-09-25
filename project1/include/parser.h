b71d1a7dfd015f5334610c040a1ba14fb20b04bc
KagaJiankui <FalkenSakurai@outlook.com>
2022-09-25 18:20:22 +0800

    rebuild using regex

--- project1/include/parser.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include<cctype>
-#include<cstring>
-
-void two_word_parser(char* str, int* examine);
-inline int flag_NaN(char c);
-inline int flag_Ee(char c);
-inline int flag_pm(char c);
--- project1/src/main.cpp
+++ project1/src/main.cpp
@@ -1,29 +1,12 @@
-#include <cstdlib>
-#include <iomanip>
-#include <iostream>
+#include <complex>
 #include <string>
-
-#include "parser.h"
-
-void printArray(int* arr, int len);
-int main(int argc, const char** argv) {
-  std::string s1 = argv[1];
-  std::string s2 = argv[2];
-  const char* l1 = s1.c_str();
-  const char* l2 = s2.c_str();
-  int* ex1 = (int*)malloc((s1.length()) * sizeof(int));
-  int* ex2 = (int*)malloc((s2.length()) * sizeof(int));
-  two_word_parser((char*)l1, ex1);
-  two_word_parser((char*)l2, ex2);
-  printArray(ex1, s1.length());
-  std::cout << std::endl;
-  printArray(ex2, s2.length());
-  std::cout << std::endl;
-  return 0;
-}
-
-void printArray(int* arr, int len) {
-  for (int i = 0; i < len; i++) {
-    std::cout << *(arr + i);
-  }
-}
+#include <vector>
+#include <regex>
+void parseToString(std::string inputString,
+                   std::vector<std::complex<double>> integerPart,
+                   std::vector<std::complex<double>> decimalPart,
+                   std::vector<std::complex<double>> indexPart, int *type) {
+  std::string integerRegExp = "([+-]?\\d+)";
+  std::string floatingPointRegExp = integerRegExp + "\\.(\\d+)";
+  std::string ieee754indexRegExp = integerRegExp+"|("+floatingPointRegExp+")[Ee]"+integerRegExp;
+                   }
